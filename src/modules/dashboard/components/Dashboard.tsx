fd5c82048ca4c030c562c2aa9bcc37e5ff824bb6
Maham Shahid <maham.shahid.18@gmail.com>
2021-10-27 22:49:51 +0200

    Improve UI

--- src/modules/dashboard/components/Dashboard.tsx
+++ src/modules/dashboard/components/Dashboard.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 
+import './Dashboard.css';
+
+import { DataPoint } from '../../../types';
 import { cakeStockData, portfolios } from '../../../constants';
 import { getAllDataPointsForMonth, getDataForFirstDayOfMonth, getDataToPopulateForMonth } from '../utils';
-import { DataPoint } from '../../../types';
+
 
 export const Dashboard = () => {
     const [monthSeries, setMonthSeries] = useState<string[]>([]);
     const [valueOfPortfolio, setValueOfPortfolio] = useState<string[]>([]);
 
     useEffect(() => {
-        const monthlySalary = 3000;
+        const monthlySalary = 2000;
         const fifteenPercentOfSalary = 0.15 * monthlySalary;
-        const toleranceLevel = 5;
+        const toleranceLevel = 1;
         const closestPortfolioValue = toleranceLevel + 1;
         const portfolioSelected = !!portfolios[toleranceLevel] ? portfolios[toleranceLevel] : portfolios[closestPortfolioValue];
 
@@ -44,24 +47,27 @@ export const Dashboard = () => {
 
     return (
         <React.Fragment>
-            <Chart
-                options={
-                    {
-                        chart: {
-                            id: "basic-bar"
-                        },
-                        xaxis: {
-                            categories: monthSeries
-                        },
-                        dataLabels: {
-                            enabled: false
+            <div className="dashboard-container">
+                <h2 className="chart-title">CAKE portfolio trend (in $)</h2>
+                <Chart
+                    className="chart"
+                    options={
+                        {
+                            chart: {
+                                id: "basic-bar"
+                            },
+                            xaxis: {
+                                categories: monthSeries
+                            },
+                            dataLabels: {
+                                enabled: false
+                            }
                         }
                     }
-                }
-                series={[{ name: 'value of portfolio', data: valueOfPortfolio }]}
-                type="bar"
-                width="1500"
-            />
+                    series={[{ name: 'value of portfolio', data: valueOfPortfolio }]}
+                    type="bar"
+                />
+            </div>
         </React.Fragment>
     );
 }
